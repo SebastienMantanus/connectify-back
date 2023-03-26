d75e38667d154214753fb1bbb9fc3ec5c9d1c585
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2023-03-26 18:51:03 +0200

    bored

--- index.js
+++ index.js
@@ -9,13 +9,6 @@ app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URI_ONLINE);
 
-// import des routes
-const usersRoutes = require("./routes/users");
-app.use(usersRoutes);
-
-const affiliatesRoutes = require("./routes/affiliates");
-app.use(affiliatesRoutes);
-
 // routes principales
 app.get("/", (req, res) => {
   res.json({ message: "Hi, welcome to my Affiliate Backend" });
@@ -28,6 +21,13 @@ app.get("/hello", (req, res) => {
 app.all("*", function (req, res) {
   res.json({ message: "Page not found" });
 });
+
+// import des routes
+const usersRoutes = require("./routes/users");
+app.use(usersRoutes);
+
+const affiliatesRoutes = require("./routes/affiliates");
+app.use(affiliatesRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server has started 🥵");
