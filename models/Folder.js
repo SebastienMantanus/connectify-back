a0cf751eb7d31452c162df8ed7e4b886ddd6f1b7
Sébastien Mantanus <sebastienmantanus@iMac-de-Sebastien.local>
2023-05-28 11:00:09 +0200

    Folder Shema ok

---
+++ models/Folder.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const User = require("./User");
+
+const shema = mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    responsable: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamp: true }
+);
+
+const Folder = mongoose.model("Folder", shema);
